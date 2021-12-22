35af6ec14bf9aa113b1f44dc26e8fafe6339c8a1
LChen <you@example.com>
2021-12-22 11:06:55 +0800

    替换高频乘法消耗cpu

--- dsserver/src/screen.rs
+++ dsserver/src/screen.rs
@@ -1,8 +1,7 @@
-
-use std::{io::ErrorKind::WouldBlock};
-use scrap::Display;
-use std::time::Duration;
 use scrap::Capturer;
+use scrap::Display;
+use std::io::ErrorKind::WouldBlock;
+use std::time::Duration;
 
 /**
  * 截屏
@@ -10,7 +9,7 @@ use scrap::Capturer;
 pub struct Cap {
     w: usize,
     h: usize,
-    step: usize,
+    org_len: usize,
     capturer: Capturer,
     sleep: Duration,
 }
@@ -20,12 +19,12 @@ impl Cap {
         let capturer = Capturer::new(display).unwrap();
         let (w, h) = (capturer.width(), capturer.height());
         Cap {
-                w,
-                h,
-                step: w * 4,
-                capturer,
-                sleep: Duration::new(1, 0) / 60,
-            }
+            w,
+            h,
+            org_len: w * h * 4,
+            capturer,
+            sleep: Duration::new(1, 0) / 60,
+        }
     }
     pub fn wh(&self) -> (usize, usize) {
         return (self.w, self.h);
@@ -49,18 +48,14 @@ impl Cap {
             };
 
             // 转换成rgb图像数组
-            let (w, h) = (self.w, self.h);
             let mut k = 0;
-            for y in 0..h {
-                for x in 0..w {
-                    let i = self.step * y + 4 * x;
-                    cap_buf[k] = buffer[i+2] & dscom::BIT_SAVE;
-                    k+=1;
-                    cap_buf[k] = buffer[i+1] & dscom::BIT_SAVE;
-                    k+=1;
-                    cap_buf[k] = buffer[i] & dscom::BIT_SAVE;
-                    k+=1;
-                }
+            let mut n = 0;
+            while n < self.org_len {
+                cap_buf[k] = buffer[n + 2] & dscom::BIT_SAVE;
+                cap_buf[k + 1] = buffer[n + 1] & dscom::BIT_SAVE;
+                cap_buf[k + 2] = buffer[n] & dscom::BIT_SAVE;
+                k += 3;
+                n += 4;
             }
             break;
         }
