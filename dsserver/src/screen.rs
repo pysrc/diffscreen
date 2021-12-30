1c2dce68655e2b866e2868e733f03fb4f4b1de53
LChen <you@example.com>
2021-12-30 10:35:56 +0800

    添加截屏失败处理

--- dsserver/src/screen.rs
+++ dsserver/src/screen.rs
@@ -10,7 +10,7 @@ pub struct Cap {
     w: usize,
     h: usize,
     org_len: usize,
-    capturer: Capturer,
+    capturer: Option<Capturer>,
     sleep: Duration,
 }
 impl Cap {
@@ -22,42 +22,69 @@ impl Cap {
             w,
             h,
             org_len: w * h * 4,
-            capturer,
+            capturer: Some(capturer),
             sleep: Duration::new(1, 0) / 60,
         }
+    }
+    fn reload(&mut self) {
+        println!("Reload capturer");
+        drop(self.capturer.take());
+        let display = match Display::primary() {
+            Ok(display) => display,
+            Err(e) => {
+                println!("{}", e);
+                return;
+            }
+        };
+
+        let capturer = match Capturer::new(display) {
+            Ok(capturer) => capturer,
+            Err(_) => return,
+        };
+        self.capturer = Some(capturer);
     }
     pub fn wh(&self) -> (usize, usize) {
         return (self.w, self.h);
     }
     pub fn cap(&mut self, cap_buf: &mut [u8]) {
         loop {
-            // Wait until there's a frame.
-            let cp = &self.capturer.frame();
-            let buffer = match cp {
-                Ok(buffer) => buffer,
-                Err(error) => {
-                    std::thread::sleep(self.sleep);
-                    if error.kind() == WouldBlock {
-                        // Keep spinning.
-                        continue;
-                    } else {
-                        println!("cap error: {}", error);
-                        continue;
+            match &mut self.capturer {
+                Some(capturer) => {
+                    // Wait until there's a frame.
+                    let cp = capturer.frame();
+                    let buffer = match cp {
+                        Ok(buffer) => buffer,
+                        Err(error) => {
+                            std::thread::sleep(self.sleep);
+                            if error.kind() == WouldBlock {
+                                // Keep spinning.
+                                continue;
+                            } else {
+                                std::thread::sleep(std::time::Duration::from_millis(200));
+                                self.reload();
+                                continue;
+                            }
+                        }
+                    };
+
+                    // 转换成rgb图像数组
+                    let mut k = 0;
+                    let mut n = 0;
+                    while n < self.org_len {
+                        cap_buf[k] = buffer[n + 2] & dscom::BIT_SAVE;
+                        cap_buf[k + 1] = buffer[n + 1] & dscom::BIT_SAVE;
+                        cap_buf[k + 2] = buffer[n] & dscom::BIT_SAVE;
+                        k += 3;
+                        n += 4;
                     }
+                    break;
+                }
+                None => {
+                    std::thread::sleep(std::time::Duration::from_millis(200));
+                    self.reload();
+                    continue;
                 }
             };
-
-            // 转换成rgb图像数组
-            let mut k = 0;
-            let mut n = 0;
-            while n < self.org_len {
-                cap_buf[k] = buffer[n + 2] & dscom::BIT_SAVE;
-                cap_buf[k + 1] = buffer[n + 1] & dscom::BIT_SAVE;
-                cap_buf[k + 2] = buffer[n] & dscom::BIT_SAVE;
-                k += 3;
-                n += 4;
-            }
-            break;
         }
     }
 }
