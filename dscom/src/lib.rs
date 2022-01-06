fb0c50c2ac7b3d3dea632a84f236c2e965c60898
LChen <you@example.com>
2022-01-07 00:09:54 +0800

    修复zstd造成的内存抖动问题

--- dscom/src/lib.rs
+++ dscom/src/lib.rs
@@ -1,5 +1,8 @@
-
 // 默认帧率
+use std::cell::RefCell;
+use zstd::block::Compressor;
+use zstd::block::Decompressor;
+
 pub const FPS: u64 = 20;
 
 // 传输像素保留位数（右边0越多压缩程度越大）
@@ -13,38 +16,44 @@ pub const KEY_UP: u8 = 1;
 pub const KEY_DOWN: u8 = 2;
 pub const MOUSE_KEY_UP: u8 = 3;
 pub const MOUSE_KEY_DOWN: u8 = 4;
-pub const MOUSE_WHEEL_UP:u8 = 5;
-pub const MOUSE_WHEEL_DOWN:u8 = 6;
-pub const MOVE:u8 = 7;
+pub const MOUSE_WHEEL_UP: u8 = 5;
+pub const MOUSE_WHEEL_DOWN: u8 = 6;
+pub const MOVE: u8 = 7;
 // key事件 end
 
-
-
-
+thread_local! {
+    static COMPRESSOR: RefCell<Compressor> = RefCell::new(Compressor::new());
+    static DECOMPRESSOR: RefCell<Decompressor> = RefCell::new(Decompressor::new());
+}
 
 pub fn compress(src: &[u8], dst: &mut Vec<u8>) -> usize {
-
-    unsafe{
+    unsafe {
         dst.set_len(0);
     }
-    zstd::stream::copy_encode(src, &mut *dst, COMPRESS_LEVEL).unwrap();
-    return dst.len();
+    return COMPRESSOR.with(|c| {
+        let mut comp = c.borrow_mut();
+        return comp
+            .compress_to_buffer(src, &mut *dst, COMPRESS_LEVEL)
+            .unwrap();
+    });
 }
 
 pub fn decompress(src: &[u8], dst: &mut Vec<u8>) -> usize {
     unsafe {
         dst.set_len(0);
     }
-    zstd::stream::copy_decode(src, &mut *dst).unwrap();
-    return dst.len();
+    return DECOMPRESSOR.with(|d| {
+        let mut dcomp = d.borrow_mut();
+        return dcomp.decompress_to_buffer(src, &mut *dst).unwrap();
+    });
 }
 
 #[cfg(test)]
 mod tests {
-use crate::{compress, decompress};
+    use crate::{compress, decompress};
 
-#[test]
-fn it_works() {
+    #[test]
+    fn it_works() {
         let s = "Hello11111111111111111111".as_bytes();
         let mut pr = Vec::with_capacity(100);
         unsafe {
